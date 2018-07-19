29a28c5b0c08974edc693ec9a5b200b2e07fb13f
satye1809 <satyendray384@gmail.com>
2018-07-19 12:35:28 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<HEAD>
+<TITLE>WebChat</TITLE>
+<link href="style.css" rel="stylesheet" type="text/css">
+</HEAD>
+<BODY>
+<div id="main">
+<div id="info">
+<h2>login here</h2>
+<form action="login.php" method="post">
+<label><b>Username:</b></label>
+<input type="text" name="uname" placeholder="User name"><br><br>
+<label><b>Password:</b></label>
+<input type="password" name="pass" placeholder="Password"><br><br>
+<button style="background-color:#6495ed;color:white;" type="submit"><b>Login</b></button>
+</form>
+<form action="signup.php" method="post">
+<h2>don't have an account signup here</h2>
+<label>Username:</label>
+<input type="text" name="uname" placeholder="User name"><br><br>
+<label>Email Add:</label>
+<input type="text" name="Email" placeholder="Email"><br><br>
+<label>Password:</label>
+<input type="password" name="Password" placeholder="Password"><br><br>
+<button style="background-color:#6495ed;color:white;" type="submit"><b>Signup</b></button>
+</form>
+<div>
+
+</div>
+</BODY>
+</HTML>
